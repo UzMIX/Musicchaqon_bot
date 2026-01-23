79ed9c9927aea3b15020c2ab376d64da9532a562
UzMIX <abdulatif2000411@gmail.com>
2026-01-24 01:18:09 +0500

    Fix token

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+TOKEN = os.getenv("7911095584:AAGSCkacDQDemD7UuVQSHhSdqopM_EHgqkM")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Musicchaqon_bot 24/7 ishlayapti 🎵")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
